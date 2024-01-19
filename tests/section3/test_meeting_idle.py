3ad86134139bfdcd6a3b15ca841ef7e2503cc5e3
larry <JLawrenceB1@gmail.com>
2024-01-19 10:06:51 +0000

    Fixed test_main_stdout

--- tests/section3/test_meeting_idle.py
+++ tests/section3/test_meeting_idle.py
@@ -1,6 +1,6 @@
 from app.section3 import meeting_idle
 
-def test_main():
+def test_main_stdout(capsys):
     meeting_idle.main()
     captured = capsys.readouterr()
     assert captured.out == "2\n115\n"
