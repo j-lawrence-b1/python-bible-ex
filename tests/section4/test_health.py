99b531286463bb7751b9f64bed22015b96134504
larry <JLawrenceB1@gmail.com>
2024-01-31 09:07:09 +0000

    Fixed a bug. Formatted.

--- tests/section4/test_health.py
+++ tests/section4/test_health.py
@@ -2,14 +2,15 @@
 
 from app.section4 import health
 
+
 def test_main_does_not_crash():
     health.main()
     assert True
 
-def test_main_difficulty_has_effect():
+
+def test_main_returns_valid_health_range(capsys):
     min_health = 49 + 25
     max_health = 49 + 50
-    ret_valhealth.main()
+    ret_val = health.main()
     ret_val = int(capsys.readouterr().out.strip())
     assert ret_val >= min_health and ret_val < max_health
-    
