a4c3276d58eca5927b608d87f4caae926dad661a
larry <JLawrenceB1@gmail.com>
2024-02-06 10:14:12 +0000

    Changed main() to return an int

--- app/section4/health.py
+++ app/section4/health.py
@@ -7,8 +7,8 @@ def main(difficulty: int = 1):
     health = 49
     potion_health = int(random.randint(24, 50) / difficulty)
     health += potion_health
-    print(health)
+    return health
 
 
 if __name__ == "__main__":
-    main()
+    print(main())
--- tests/section4/test_health.py
+++ tests/section4/test_health.py
@@ -12,11 +12,13 @@ def test_can_main():
         assert True
 
 
-def test_main_returns_valid_health_range(capsys):
+def test_health(capsys):
+    """
+    Test whether health is returned within the min (25) and max(49) values.
+    """
     min_health = 49 + 25
     max_health = 49 + 50
     ret_val = health.main()
-    ret_val = int(capsys.readouterr().out.strip())
     assert ret_val >= min_health and ret_val < max_health
 
 
@@ -25,13 +27,8 @@ def test_difficulty(capsys):
     Test whether, on average, increased difficulty
     results in diminished health.
     """
-    for outer in range(10):
-        ret_val1 = 0
-        ret_val2 = 0
-        for inner in range(10):
-            health.main(1)
-            ret_val1 += int(capsys.readouterr().out.strip())
-        for _ in range(10):
-            health.main(2)
-            ret_val2 += int(capsys.readouterr().out.strip())
-        assert ret_val1 > ret_val2
+    for _ in range(10):
+        vals1 = [health.main(1) for _ in range(10)]
+        vals2 = [health.main(2) for _ in range(10)]
+        vals3 = [health.main(3) for _ in range(10)]
+        assert sum(vals1) > sum(vals2) and sum(vals2) > sum(vals3)
