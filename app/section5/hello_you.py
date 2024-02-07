e07e3af4fffe2bd9ec816c2dd63684d7997642a5
larry <JLawrenceB1@gmail.com>
2024-02-07 09:46:05 +0000

    Added hello_you

--- app/section5/hello_you.py
+++ app/section5/hello_you.py
@@ -1,56 +1,70 @@
 #!/usr/bin/env python3
 
-'''Module docstring. '''
+"""Module docstring. """
 
 
-def prompt(prompt: str = '', default: str = '') -> str:
-    '''
+def prompt(ppt: str = "", default: str = "") -> str:
+    """
     Prompt for console input.
 
     Keyword arguments:
     prompt -- Desired input hint (default: '').
     default -- Default input value (default: '').
 
-    '''
-    
-    retval = input(f'{prompt}? [{default}]: ')
-    if input is None:
+    """
+
+    retval = input(f"{ppt}? [{default}]: ")
+    if retval is None:
         retval = default
 
     return retval.strip()
 
 
-# Ask user for name.
-
-# Ask user for age.
-
-# Ask user for city.
-
-# Ask user what they enjoy.
-
-# Create output text.
-
-# Print output to screen.
-
-
-def hello():
-    '''Say, "Hello".'''
-
-    user = prompt('Howdy, neighbor! What is your name')
-    return f'Hello, {user}!'
-
-
 def main():
-    '''The main thing.'''
+    name = None
+    age = None
+    city = None
+    activity = None
 
-    return hello()
+    # Ask user for name.
+    while not name:
+        retval = prompt("What is your name")
+        if len(retval) > 0:
+            name = retval
+
+    # Ask user for age.
+    while not age:
+        retval = prompt("What is your age")
+        if retval.isnumeric():
+            age = int(retval)
+
+    # Ask user for city.
+    while not city:
+        retval = prompt("What city were you born in")
+        if len(retval) > 0:
+            city = retval
+
+    # Ask user what they enjoy.
+    while not activity:
+        retval = prompt("What is your favorite activity")
+        if len(retval) > 0:
+            activity = retval
+
+    # Create output text.
+    output_text = (
+        f"Hello, {name}. I see you are {age} years old, "
+        + f"were born in {city}, and love {activity}."
+    )
+
+    # Print output to screen.
+    return output_text
 
 
-if __name__ == '__main__':
-    '''
+if __name__ == "__main__":
+    """
     Entry point.
 
     OPEN ISSUE: Should I upgrade to setuptools and setup.py?
-    '''
+    """
 
     print(main())
--- tests/section5/test_hello_you.py
+++ tests/section5/test_hello_you.py
@@ -1,22 +1,30 @@
 #!/usr/bin/env python
 
+"""Test the hello_you module"""
+
+from io import StringIO
 from app.section5 import hello_you
 
 
-class TestsHelloYou:
-    """Test the hello_you module"""
-
-    def test_can_main():
-        try:
-            hello_you.main()
-        except AttributeError:
-            assert False
-        finally:
-            assert True
-
-    def test_prompt(mocker):
-        with mocker.patch.object(__builtins__, "input", lambda: "San Francisco"):
-            assert hello_you.prompt("Enter City") == "San Franciscox"
-
-    def test_main():
+def test_can_main():
+    try:
+        hello_you.main()
+    except AttributeError:
         assert False
+    finally:
+        assert True
+
+
+def test_prompt(monkeypatch):
+    inputs = StringIO("San Francisco\n")
+    monkeypatch.setattr("sys.stdin", inputs)
+    assert hello_you.prompt("Enter City") == "San Francisco"
+
+
+def test_main(monkeypatch):
+    inputs = StringIO("Bobby\n3\nSF\ndiving\n")
+    monkeypatch.setattr("sys.stdin", inputs)
+    assert hello_you.main() == (
+        "Hello, Bobby. I see you are 3 years old," +
+        " were born in SF, and love diving."
+    )
