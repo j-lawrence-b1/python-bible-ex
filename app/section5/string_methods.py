5eb3b8e50f21e29a9e6b623a93cecb0ea44416ed
larry <JLawrenceB1@gmail.com>
2024-02-07 10:45:38 +0000

    Added main function

--- app/section5/string_methods.py
+++ app/section5/string_methods.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python3
 
-"""Module docstring. """
+'''Module docstring. '''
 
 
-def main():
+def main()'
+    output = StringIO('')
+    # output now '1\n'
+    print(output, 'hello'.count('e'))
+    return(output)
+
+
     pass
 
 
-if __name__ == "__main__":
-    """
+if __name__ == '__main__'
+    '''
     Entry point.
 
-    OPEN ISSUE: Should I upgrade to setuptools and setup.py?
-    """
+    OPEN ISSUE' Should I upgrade to setuptools and setup.py?
+    '''
 
     print(main())
