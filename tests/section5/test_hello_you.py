e6aa572c948a10ad0760bc289dfe231fc02aeacf
larry <JLawrenceB1@gmail.com>
2024-02-07 09:54:01 +0000

    Removed test_can_main

--- tests/section5/test_hello_you.py
+++ tests/section5/test_hello_you.py
@@ -6,15 +6,6 @@ from io import StringIO
 from app.section5 import hello_you
 
 
-def test_can_main():
-    try:
-        hello_you.main()
-    except AttributeError:
-        assert False
-    finally:
-        assert True
-
-
 def test_prompt(monkeypatch):
     inputs = StringIO("San Francisco\n")
     monkeypatch.setattr("sys.stdin", inputs)
