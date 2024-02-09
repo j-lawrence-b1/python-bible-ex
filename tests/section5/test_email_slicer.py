a5c7f414e4abad2758d2fdc115bc21506970b47b
larry <JLawrenceB1@gmail.com>
2024-02-09 09:10:54 +0000

    Added remaining email_slicer tests.

--- tests/section5/test_email_slicer.py
+++ tests/section5/test_email_slicer.py
@@ -2,6 +2,10 @@
 
 """Test the email_slicer module"""
 
+"""Import Python modules."""
+from io import StringIO
+
+"""Import Application modules."""
 from src.section5 import email_slicer
 
 
@@ -18,7 +22,51 @@ def test_can_main():
         pass
 
 
-def test_main():
-    """Test if main() runs without error"""
+def test_get_email_success(monkeypatch):
+    """Test success."""
+    inputs = StringIO("some.body@example.com\n")
+    monkeypatch.setattr("sys.stdin", inputs)
+    assert email_slicer.get_email() == "some.body@example.com"
 
+
+def test_get_email_eventual_success(monkeypatch):
+    """Test success."""
+    inputs = StringIO("some-bad-email-address\nsome.body@example.com\n")
+    monkeypatch.setattr("sys.stdin", inputs)
+    assert email_slicer.get_email() == "some.body@example.com"
+
+
+def test_get_name():
+    """Test extracting name from email."""
+    assert email_slicer.get_name("some.body@example.com") == "some.body"
+
+
+def test_get_domain():
+    """Test extracting domain name from email."""
+    assert email_slicer.get_domain("some.body@example.com") == "example.com"
+
+
+def test_format_output():
+    """Test constructing the output message."""
+    assert email_slicer.format_output("some.body", "example.com") == (
+        "Your name is some.body and your domain is example.com."
+    )
+
+
+def test_display_output(capsys):
+    """Test printing the output message to the console."""
+    msg = "Your name is some.body and your domain is example.com."
+    email_slicer.display_output(msg)
+    captured = capsys.readouterr()
+    assert captured.out.strip() == msg
+
+
+def test_main(monkeypatch, capsys):
+    """Test if main() runs without error"""
+    inputs = StringIO("some.body@example.com\n")
+    # sys.capsys swallows the newline after the prompt.
+    expected = "What is your email address? []: Your name is some.body and your domain is example.com.\n"
+    monkeypatch.setattr("sys.stdin", inputs)
     email_slicer.main()
+    captured = capsys.readouterr()
+    assert captured.out == expected
